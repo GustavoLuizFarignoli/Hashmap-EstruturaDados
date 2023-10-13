f47b36a7212776c85d02be3f9de1657e8a5280ec
GustavoLuizFarignoli <gustavolf.educ@gmail.com>
2023-10-13 10:02:52 -0300

    Teste Remover Hash1 e esqueleto func remover do main

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,37 @@ import Hash.Hash1;
 
 public class Main {
     public static void main(String[] args) {
-        Hash1 hash1 = new Hash1(3);
+        Hash1 hash1 = new Hash1(7);
         Aluno a = new Aluno(1,"Marcelo");
         hash1.inserir(a);
         Aluno b = new Aluno(10,"João");
         hash1.inserir(b);
         Aluno c = new Aluno(5,"Molecote");
         hash1.inserir(c);
-        Aluno n = new Aluno("A","Vittorio");
+        Aluno n = new Aluno("C","Vittorio");
         hash1.inserir(n);
 
-
         for (int i = 0; i < hash1.getTabela().length; i++) {
-            System.out.println(i+1 + " - " + hash1.getTabela()[i]);
+            System.out.println(i + " - " + hash1.getTabela()[i]);
         }
+
     }
 
-    public void buscaraluno (String s,boolean usarid){
-        Hash1 hash1 = new Hash1(7);
+    public static void buscaraluno (String s,boolean usarid, Hash1 hash1){
         Aluno pesquisa = hash1.buscar(s,usarid);
         if (pesquisa == null){
             System.out.println("Aluno não encontrado");
         } else {
             System.out.println(pesquisa.getNome());
+        }
+    }
+
+    public static void removeraluno (String s,boolean usarid, Hash1 hash1){
+        Aluno removido = hash1.remover(s,usarid);
+        if (removido == null){
+            System.out.println("Aluno não encontrado");
+        } else {
+            System.out.println(removido.getNome());
         }
     }
 }
